340afe5640ed89d783388ac8d5688a1dfc839608
trevthedev777 <bloodsportrecords@gmail.com>
2022-05-30 15:35:56 +0000

    Wire API's, test connection to google sheets

--- run.py
+++ run.py
@@ -1,10 +1,30 @@
-print('')
-print('============================')
-print('============================')
-print('Welcome To Your Contact Book')
-print('============================')
-print('============================')
-print('')
-print('Contacts are loading....')
-print('')
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('contactBook')
+
+# Access the contact information
+directory = SHEET.worksheet('Directory')
+contactInfo = directory.get_all_values()
+
+print(contactInfo)
+
+# print('')
+# print('============================')
+# print('============================')
+# print('Welcome To Your Contact Book')
+# print('============================')
+# print('============================')
+# print('')
+# print('Contacts are loading....')
+# print('')
 
